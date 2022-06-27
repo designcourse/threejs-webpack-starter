b64c8eac8e9e426528bd96ff51854252d2dd5998
Jesse Stratford <98693587+jessestratford92@users.noreply.github.com>
2022-06-27 19:30:52 +1200

    dat gui x,y,z and intensity added

--- src/script.js
+++ src/script.js
@@ -41,6 +41,21 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
+const pointLight2 = new THREE.PointLight(0xff0000, 2)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+pointLight2.position.set(1,1,1)
+pointLight2.intensity = 1 
+
+scene.add(pointLight2)
+
+gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLight2.position, 'x').min(-3).max(3).step(0.01)
+gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+gui.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
+
+
 /**
  * Sizes
  */
